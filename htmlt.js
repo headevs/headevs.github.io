d67cf603798b16c1367c0c57ef5c8e9cf520dcce
headevs <124409098+headevs@users.noreply.github.com>
2023-11-27 20:54:07 +0800

    not final update 11/27/23

--- htmlt.js
+++ htmlt.js
@@ -3,3 +3,9 @@ var dmButton = document.getElementById("dmButton");
 dmButton.onclick = function() {
     document.body.classList.toggle("switch-theme");
 }
+
+var dropdownButton = document.getElementById("dropdownButton");
+
+dropdownButton.onclick = function() {
+    document.body.classList.toggle("left-nav-close");
+}
