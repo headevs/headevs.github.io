66ad78976e9f058fcbc78096f8c5765987cc2d11
headevs <124409098+headevs@users.noreply.github.com>
2023-12-15 20:00:53 +0800

    Buttons added Update

--- htmlt.js
+++ htmlt.js
@@ -31,17 +31,70 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-function toggleCodeContainer(containerNumber) {
-    var codeContainer = document.getElementById("codeContainer" + containerNumber);
-    var maxHeight = codeContainer.style.maxHeight;
+        function toggleCodeContainer(containerNumber) {
+            var codeContainer = document.getElementById("codeContainer" + containerNumber);
+            var copyButton = document.getElementById('buttonGroup' + containerNumber).querySelector('.copyButton');
+            var outputButton = document.getElementById('buttonGroup' + containerNumber).querySelector('.outputButton');
+            var downloadButton = document.getElementById('buttonGroup' + containerNumber).querySelector('.downloadButton');
 
-    if (maxHeight === "0px" || maxHeight === "") {
-        codeContainer.style.display = "block";
-        codeContainer.style.maxHeight = codeContainer.scrollHeight + "px";
-    } else {
-        codeContainer.style.maxHeight = "0";
-        setTimeout(function () {
-            codeContainer.style.display = "none";
-        }, 300);
-    }
-}
+            var maxHeight = codeContainer.style.maxHeight;
+
+            if (maxHeight === "0px" || maxHeight === "") {
+                codeContainer.style.display = "block";
+                codeContainer.style.maxHeight = codeContainer.scrollHeight + "px";
+                copyButton.style.display = "inline";
+                outputButton.style.display = "inline";
+                downloadButton.style.display = "inline";
+            } else {
+                codeContainer.style.maxHeight = "0";
+                setTimeout(function () {
+                    codeContainer.style.display = "none";
+                    copyButton.style.display = "none";
+                    outputButton.style.display = "none";
+                    downloadButton.style.display = "none";
+                }, 300);
+            }
+        }
+
+        function copyCode(containerNumber) {
+            var codeContainer = document.getElementById("codeContainer" + containerNumber);
+            var codeToCopy = codeContainer.querySelector('code').innerText;
+
+            var tempTextarea = document.createElement('textarea');
+            tempTextarea.value = codeToCopy;
+            document.body.appendChild(tempTextarea);
+
+            tempTextarea.select();
+            document.execCommand('copy');
+
+            document.body.removeChild(tempTextarea);
+
+            alert('Code copied to clipboard!');
+        }
+
+        function runCode(containerNumber) {
+            var codeContainer = document.getElementById("codeContainer" + containerNumber);
+            var codeToRun = codeContainer.querySelector('code').innerText;
+
+            var blob = new Blob(['<!DOCTYPE html><html>' + codeToRun + '</html>'], { type: 'text/html' });
+
+            var outputWindow = window.open(URL.createObjectURL(blob), '_blank');
+
+        }
+
+        function downloadCode(containerNumber) {
+            var codeContainer = document.getElementById("codeContainer" + containerNumber);
+            var codeToDownload = codeContainer.querySelector('code').innerText;
+
+            var blob = new Blob(['<!DOCTYPE html><html>' + codeToDownload + '</html>'], { type: 'text/html' });
+
+            var link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'output.html';
+
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+
+        }
